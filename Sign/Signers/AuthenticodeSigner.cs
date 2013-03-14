26eac6a0d8e166ec0522a5bdf27a14495a0a7e45
Eric Schultz <wwahammy@gmail.com>
2013-03-14 11:48:35 -0500

    Initial Commit

---
+++ Sign/Signers/AuthenticodeSigner.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using ClrPlus.Windows.PeBinary.Utility;
+
+namespace ClrPlus.Signing.Signers
+{
+    
+
+    public class AuthenticodeSigner
+    {
+        private
+        const BinaryLoadOptions BINARY_LOAD_OPTIONS = BinaryLoadOptions.PEInfo |
+            BinaryLoadOptions.VersionInfo |
+            BinaryLoadOptions.Managed |
+            BinaryLoadOptions.Resources |
+            BinaryLoadOptions.Manifest |
+            BinaryLoadOptions.UnsignedManagedDependencies |
+            BinaryLoadOptions.MD5;
+
+        public AuthenticodeSigner(X509Certificate2 certificate) {
+            Certificate = certificate;
+        }
+        public X509Certificate2 Certificate {get; private set;}
+
+        public void Sign(string path, bool strongName = false)
+        {
+            var certRef = new CertificateReference(Certificate);
+            var r = BinaryLoad(path);
+
+            r.SigningCertificate = certRef;
+            if (strongName)
+                r.StrongNameKeyCertificate = certRef;
+
+            r.Save().Wait();
+        }
+
+        private Binary BinaryLoad(string path)
+        {
+            return Binary.Load(path, BINARY_LOAD_OPTIONS).Result;
+        }
+    }
+}
---
+++ Sign/Signers/OPCSigner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClrPlus.Signing.Signers
+{
+    public class OPCSigner 
+    {
+        public OPCSigner(X509Certificate2 certificate) {
+            Certificate = certificate;
+        }
+
+        public X509Certificate2 Certificate {get; private set;}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <from>http://msdn.microsoft.com/en-us/library/system.io.packaging.packagedigitalsignaturemanager.sign(v=vs.100).aspx</from>
+        public void Sign(string path) {
+            var package = Package.Open(path);
+
+
+            var signatureManager = new PackageDigitalSignatureManager(package);
+            signatureManager.CertificateOption = CertificateEmbeddingOption.InSignaturePart;
+
+            var toSign = new List<Uri>();
+            foreach (PackagePart packagePart in package.GetParts())
+            {
+                toSign.Add(packagePart.Uri);
+            }
+
+            toSign.Add(PackUriHelper.GetRelationshipPartUri(signatureManager.SignatureOrigin));
+            toSign.Add(signatureManager.SignatureOrigin);
+            toSign.Add(PackUriHelper.GetRelationshipPartUri(new Uri("/", UriKind.RelativeOrAbsolute)));
+
+           
+                signatureManager.Sign(toSign, Certificate);
+                package.Close();
+           
+        }
+    }
+}
